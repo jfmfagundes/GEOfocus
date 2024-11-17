2eb259a65668dd052bfdbd5dd436250f00b4263a
Joao Fagundes <jfmfagundes@hotmail.com>
2024-11-17 16:46:24 -0300

    Changes to be committed: 	modified:   content/0_Home.py

--- content/0_Home.py
+++ content/0_Home.py
@@ -7,7 +7,7 @@ st.title("Análises de Dados Geoespaciais")
 
 st.markdown(
     """
-    Este aplicativo web permite realizar análises com base em dados geoespaciais do _Infinity Tracker_.   
+    Este aplicativo web permite realizar análises com base em dados geoespaciais do .   
     Trata-se de projeto experimental, sujeito a eventuais _bugs_ e incorreções.
     """
 )
@@ -15,7 +15,7 @@ st.markdown(
 st.header("Instruções")
 
 markdown = """
-1. Para iniciar você precisará do(s) arquivo(s) exportado(s) pelo _Tracker_.   
+1. Para iniciar você precisará do(s) arquivo(s) exportado(s) pelo GPS.   
 2. No Menu de Navegação à esquerda, procure a página de [Upload]() e envie o(s) arquivo(s) de interesse.
 3. Após fazer o _upload_ dos dados, basta navegar pelas diferentes opções de análises.
 4. As análises podem ser realizadas em qualquer ordem, bastando inicial fazer o _upload_ do arquivo a se estudado.
